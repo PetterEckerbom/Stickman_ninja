9de2fe1a342ca1b94603990069f4cf2141f62f67
PetterEckerbom <31358711+PetterEckerbom@users.noreply.github.com>
2017-09-01 14:09:09 +0200

    removed a comment

--- communication.js
+++ communication.js
@@ -7,7 +7,6 @@ exports.move_change = function(socket, dir){
   	var games_check = matchmaking.findplayer(matchmaking.STARTED_GAMES, socket.id);
       if(games_check.index != -1){
         if(matchmaking.STARTED_GAMES[games_check.index][games_check.Player].dir != dir){
-          //console.log(matchmaking.STARTED_GAMES[games_check.index][games_check.Player].dir)
           matchmaking.STARTED_GAMES[games_check.index][games_check.Player].dir = dir;
           matchmaking.STARTED_GAMES[games_check.index][games_check.Player].socket.emit('Change_direction_you', dir);
           matchmaking.STARTED_GAMES[games_check.index][games_check.NotPlayer].socket.emit('Change_direction_enemy', dir);
