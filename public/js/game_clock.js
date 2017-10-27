e642e7888a7522dd846fb52e2b67c8923b06e6f5
PetterEckerbom <31358711+PetterEckerbom@users.noreply.github.com>
2017-10-27 10:40:01 +0200

    Added back punch up system stuff

--- public/js/game_clock.js
+++ public/js/game_clock.js
@@ -1,25 +1,13 @@
 setInterval(function () {
   ctx.clearRect(0, 0, c.width, c.height);
-  //Temp way of drawing map elements
-  ctx.fillStyle="#ff0026";
-  for(var i=0;i<platform.length;i++){
-  ctx.fillRect(platform[i].xstart+xoffset,platform[i].y+yoffset,(platform[i].xend - platform[i].xstart),platform[i].thickness);
-  }
-  ctx.fillStyle="#00f735";
-  for(var y=0;y<walls.length;y++){
-  ctx.fillRect(walls[y].x+xoffset,walls[y].ystart+yoffset,walls[y].thickness,walls[y].yend - walls[y].ystart);
-  }
-  ctx.fillStyle="#0c00ff";
-  for(var x=0;x<jumppad.length;x++){
-  ctx.fillRect(jumppad[x].x+xoffset,jumppad[x].y+yoffset,jumppad[x].width,20);
-  }
+  draw_map()
   //calls functions that should be updated every tick and fuctions that may change outcome of the other function calls
   move_change();
   move_players();
   move_down();
   draw_players();
   find_animation(players[0]);
-  find_animation(players[1]);
+  find_animation(players[1]);ss
   //Movement
 
 },1000/30);
